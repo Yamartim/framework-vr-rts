37774573ca8be7d66accda1616337d7ba8a2dae7
Martim Lima <54243954+Yamartim@users.noreply.github.com>
2025-05-31 02:29:06 -0300

    possible passthrough implementation

--- scripts/player_vr.gd
+++ scripts/player_vr.gd
@@ -9,6 +9,8 @@ var chamar_selecionados := false
 func _ready():
 	if !Engine.is_editor_hint() and initialize:
 		$StartXR.initialize()
+		var passthrough_on = enable_passthrough()
+		print("passthrough rodando: ", passthrough_on)
 
 func _process(_delta):
 	mao_pra_cima()
@@ -35,3 +37,18 @@ func _on_mao_esq_button_pressed(name):
 	#print('mao esquerda apertou: ', name)
 	if name == 'grip_click' and chamar_selecionados:
 		%diretor.emitir_ordem(self.position)
+
+func enable_passthrough() -> bool:
+	var xr_interface: XRInterface = XRServer.primary_interface
+	if xr_interface and xr_interface.is_passthrough_supported():
+		if !xr_interface.start_passthrough():
+			return false
+	else:
+		var modes = xr_interface.get_supported_environment_blend_modes()
+		if xr_interface.XR_ENV_BLEND_MODE_ALPHA_BLEND in modes:
+			xr_interface.set_environment_blend_mode(xr_interface.XR_ENV_BLEND_MODE_ALPHA_BLEND)
+		else:
+			return false
+
+	get_viewport().transparent_bg = true
+	return true
